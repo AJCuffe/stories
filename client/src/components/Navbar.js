b33d4cbee2cc12877b9f53b462429a2e20c61cdd
Adam Cuffe <adamcuffe@Adams-MBP.lan>
2019-06-10 22:48:04 +0100

    add style navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
-const Navbar = () => <div>Navbar component</div>;
+const Navbar = () => (
+  <nav>
+    <ul>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/search">Search</NavLink>
+      </li>
+      <li>
+        <NavLink to="/signin">Login</NavLink>
+      </li>
+      <li>
+        <NavLink to="/signup">Register</NavLink>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default Navbar;
