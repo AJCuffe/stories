f63a91b1f1bf4f34baa70610c487a2fc9c9baae5
Adam Cuffe <adamcuffe@Adams-MBP.lan>
2019-06-12 00:57:29 +0100

    Login view in client

--- client/src/components/Auth/Signin.js
+++ client/src/components/Auth/Signin.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import { SIGNIN_USER } from "../../queries";
+import { withRouter } from "react-router-dom";
+import Error from "../Error";
 
 class Signin extends Component {
   constructor(props) {
@@ -8,43 +12,71 @@ class Signin extends Component {
       password: ""
     };
     this.handleChange = this.handleChange.bind(this);
+    this.validateForm = this.validateForm.bind(this);
   }
 
   handleChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
 
+  validateForm(e) {
+    const { username, password } = this.state;
+    // Validate required inputs
+    return !username || !password;
+  }
+
   render() {
     const { username, password } = this.state;
     return (
       <div className="content">
-        <form
-          className="form"
-          onSubmit={e => {
-            e.preventDefault();
-            console.log("Submitted");
-          }}
-        >
-          <h2>Sign In</h2>
-          <input
-            type="text"
-            name="username"
-            placeholder="Username"
-            value={username}
-            onChange={this.handleChange}
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={password}
-            onChange={this.handleChange}
-          />
-          <button type="submit">Submit</button>
-        </form>
+        <Mutation mutation={SIGNIN_USER}>
+          {(signinUser, { data, loading, error }) => (
+            <form
+              className="form"
+              onSubmit={async e => {
+                e.preventDefault();
+                // Send data to server
+                const { data } = await signinUser({
+                  variables: {
+                    username,
+                    password
+                  }
+                });
+                // Save token to Local Storage
+                localStorage.setItem("token", data.signinUser.token);
+                // Reset current User
+                await this.props.refetch();
+                // Clear state
+                this.setState({ username: "", password: "" });
+                // Redirect to Home Page
+                this.props.history.push("/");
+              }}
+            >
+              <h2>Sign In</h2>
+              <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                value={username}
+                onChange={this.handleChange}
+              />
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={password}
+                onChange={this.handleChange}
+              />
+              <button type="submit" disabled={loading || this.validateForm()}>
+                Submit
+              </button>
+              {error ? <Error message={error.message.split(":")[1]} /> : ""}
+            </form>
+          )}
+        </Mutation>
       </div>
     );
   }
 }
 
-export default Signin;
+export default withRouter(Signin);
